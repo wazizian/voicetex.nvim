668ab0bef27a4c0577fd72c271da97f148b14839
Waiss Azizian <waiss.azizian@univ-grenoble-alpes.fr>
2024-08-30 14:57:39 +0200

    feat: Add support for adding context from buffers in postprocessor

--- postprocessor.py
+++ postprocessor.py
@@ -1,6 +1,5 @@
 import abc
 import logging
-from openai import OpenAI
 import claudette
 
 class PostProcessor(abc.ABC):
@@ -12,65 +11,18 @@ class PostProcessor(abc.ABC):
     def add_context(self, context):
         pass
 
+    def add_context_from_buffers(self, *buffers_names, *buffers):
+        context = ""
+        for buffer_name, buffer in zip(buffers_names, buffers):
+            context += f"<BUFFER {buffer_name}>\n{buffer}\n</BUFFER>\n"
+        self.add_context(context)
+
     def add_context_from_files(self, *file_names):
         context = ""
         for file_name in file_names:
             with open(file_name, "r") as file:
-                context += file.read()
+                context += f"<FILE {file_name}>\n{file.read()}\n</FILE>\n"
         self.add_context(context)
-        
-    
-class GPTPostProcessor(PostProcessor):
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.INFO)
-        handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-        self.logger.addHandler(handler)
-
-        self.client = OpenAI()
-        self.temperature = 0.
-
-        self.system_prompt = """
-        You are a helpful assistant for a mathematician.
-        Your task is to transform the following text into valid LaTeX code.
-        In math mode, words refer to macros and symbols.
-        For example, "alpha" should be converted to "\alpha".
-
-        Preferences:
-        - Consider that you are in text mode by default.
-        - Assume that the output will be inserted in text mode between \begin{document} and \end{document}.
-        - Use the "amsmath" package.
-
-        """
-    
-    def add_context(self, context):
-        fcontext = f"""Follow closely the LaTeX code provided in the examples below.
-        <EXAMPLES>
-        {context}
-        </EXAMPLES>"""
-        self.system_prompt += fcontext
-
-    def postprocess_transcription(self, transcription):
-        self.logger.info("Sending transcription to OpenAI for postprocessing")
-        response = self.client.chat.completions.create(
-            model="gpt-4o",
-            temperature=self.temperature,
-            messages=[
-                {
-                    "role": "system",
-                    "content": self.system_prompt
-                },
-                {
-                    "role": "user",
-                    "content": transcription
-                }
-            ]
-        )
-        self.logger.info(f"Postprocessed transcription: {response.choices[0].message.content}")
-        self.logger.info(f"Usage: {response.usage}")
-        return response.choices[0].message.content
-
 
 class ClaudePostProcessor(PostProcessor):
     def __init__(self):
@@ -95,21 +47,32 @@ class ClaudePostProcessor(PostProcessor):
         - Output only the LaTeX code for the content itself.
         - Assume the amsmath package is available for use.
         - When the word "equation" is mentioned, use the equation environment (\begin{equation} ... \end{equation}).
+        - When the word "align" is mentioned, use the equation environment (\begin{align} ... \end{align}).
         - For inline math, use $...$ delimiters.
 
         """
         self.chat = claudette.Chat(self.model, sp=self.system_prompt)
     
     def add_context(self, context):
-        fcontext = f"""Follow closely the LaTeX code provided in the examples below.
+        fcontext = f"""Take inspiration from the LaTeX code provided in the examples below.
         <EXAMPLES>
         {context}
         </EXAMPLES>"""
         msg = claudette.mk_msg(fcontext, cache=True)
 
-    def postprocess_transcription(self, transcription):
+    def postprocess_transcription(self, transcription, local_context=None):
         self.logger.info("Sending transcription to Claude for postprocessing")
-        response = self.chat(transcription)
+        msg = ""
+        if local_context:
+            flocal_context = f"""Your answer will be inserted into the following context:
+            <LOCAL_CONTEXT>
+            {local_context[0]}
+            <YOUR_ANSWER>
+            {local_context[1]}
+            </LOCAL_CONTEXT>\n"""
+            msg += flocal_context
+        msg += transcription
+        response = self.chat(msg)
         latex_code = response.content[0].text.strip()
         
         self.logger.info(f"Postprocessed transcription: {latex_code}")
