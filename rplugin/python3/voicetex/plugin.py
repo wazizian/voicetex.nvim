7de4f9181eb0aa03621f3fccd0c06a2262d03961
Waiss Azizian <waiss.azizian@univ-grenoble-alpes.fr>
2024-08-31 16:18:59 +0200

    feat: add VoiceTexInit command to initialize plugin

--- rplugin/python3/voicetex/plugin.py
+++ rplugin/python3/voicetex/plugin.py
@@ -8,6 +8,11 @@ class VoiceTex:
     def __init__(self, nvim: pynvim.api.Nvim):
         self.nvim = nvim
 
+    @neovim.command("VoiceTexInit")
+    def init(self) -> None:
+        self.local_context_length = 5
+        self.nvim.command("echom 'MyPlugin: Initialized!'")
+
     @neovim.command("ModuleHelloWorld")
     def hello_world(self) -> None:
         self.nvim.command("echom 'MyPlugin: Hello World!'")
